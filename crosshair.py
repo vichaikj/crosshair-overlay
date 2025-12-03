e1bd132a47564f696d58c176b8bd9b629f8bf7b3
vichaikj <jean-philippe.vichaikit@hotmail.com>
2025-12-03 03:56:20 +0100

    Improve readability

--- crosshair.py
+++ crosshair.py
@@ -18,7 +18,7 @@ ICO_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "crosshair.i
 OFFSET = False
 
 # Default option
-SELECTED_OPTION = "medium"
+CROSSHAIR_SIZE = "medium"
 CROSSHAIR_COLOR = "green"
 
 # Size definitions
@@ -37,10 +37,10 @@ colors = {
 }
 
 
-def apply_option(option):
+def apply_size(option):
     """Update global drawing parameters"""
-    global SELECTED_OPTION, line_length, hole_size, thickness, transparency
-    SELECTED_OPTION = option
+    global CROSSHAIR_SIZE, line_length, hole_size, thickness, transparency
+    CROSSHAIR_SIZE = option
     line_length = sizes[option]["line_length"]
     hole_size = sizes[option]["hole_size"]
     thickness = sizes[option]["thickness"]
@@ -55,7 +55,7 @@ def apply_color(name):
 
 
 # Default apply
-apply_option(SELECTED_OPTION)
+apply_size(CROSSHAIR_SIZE)
 apply_color(CROSSHAIR_COLOR)
 
 
@@ -127,28 +127,11 @@ class TrayIcon(QSystemTrayIcon):
         # Context menu
         menu = QMenu(parent)
 
-        # ---- SIZE SUBMENU ----
-        self.size_menu = QMenu("Size")
-        for size in sizes.keys():
-            action = QAction(size.capitalize(), checkable=True)
-            action.setChecked(size == CROSSHAIR_COLOR)
-            action.triggered.connect(lambda _, n=size: self.switch_size(n))
-            self.size_menu.addAction(action)
-
-            setattr(self, f"{size}_action", action)
+        # ---- CREATE MENUS ----
+        self.size_menu = self._create_menu("Size", sizes.keys(), self.switch_size, CROSSHAIR_SIZE)
+        self.color_menu = self._create_menu("Color", colors.keys(), self.switch_color, CROSSHAIR_COLOR)
 
         menu.addMenu(self.size_menu)
-
-        # ---- COLOR SUBMENU ----
-        self.color_menu = QMenu("Color")
-        for color in colors.keys():
-            action = QAction(color.capitalize(), checkable=True)
-            action.setChecked(color == CROSSHAIR_COLOR)
-            action.triggered.connect(lambda _, n=color: self.switch_color(n))
-            self.color_menu.addAction(action)
-
-            setattr(self, f"{color}_action", action)
-
         menu.addMenu(self.color_menu)
 
         # ---- EXIT ----
@@ -158,22 +141,32 @@ class TrayIcon(QSystemTrayIcon):
 
         self.setContextMenu(menu)
 
+    def _create_menu(self, title, options_list, callback, selected_option):
+        """Helper to create a QMenu with checkable actions."""
+        menu = QMenu(title)
+        for opt in options_list:
+            action = QAction(opt.capitalize(), checkable=True)
+            action.setChecked(opt == selected_option)
+            action.triggered.connect(lambda _, o=opt: callback(o))
+            menu.addAction(action)
+            # store as attribute for easy access
+            setattr(self, f"{opt}_action", action)
+        return menu
+
     def switch_size(self, size_option):
-        apply_option(size_option)
+        apply_size(size_option)
         for size in sizes.keys():
             getattr(self, f"{size}_action").setChecked(size == size_option)
-
         self.overlay.refresh()
 
     def switch_color(self, color_option):
         apply_color(color_option)
         for color in colors.keys():
             getattr(self, f"{color}_action").setChecked(color == color_option)
-
         self.overlay.refresh()
 
     def exit_app(self):
-        QApplication().quit
+        QApplication.instance().quit()
 
 
 if __name__ == "__main__":
